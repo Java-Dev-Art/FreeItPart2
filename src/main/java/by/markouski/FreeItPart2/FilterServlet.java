e1711ab870fdaa2e6ed2387063e97eab336cfdcb
Markouski Artur <veles.mk.12@gmail.com>
2021-08-11 20:51:42 +0300

    homeWork/filter/3

--- src/main/java/by/markouski/FreeItPart2/FilterServlet.java
+++ src/main/java/by/markouski/FreeItPart2/FilterServlet.java
@@ -1,6 +1,8 @@
 package by.markouski.FreeItPart2;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import sun.security.pkcs11.wrapper.Constants;
 
 import javax.servlet.*;
@@ -9,24 +11,31 @@ import java.io.IOException;
 
 public class FilterServlet implements Filter {
     private static final String name = "admin";
+    private static Logger logger = LogManager.getRootLogger();
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("init filter!");
+        System.out.println("Init filter!");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletRequest req2 = (HttpServletRequest)servletRequest;
+
         RequestDispatcher dispatcher = req2.getRequestDispatcher("/forward");
+
         String username = req.getParameter("username");
+        logger.info(username);
+
         if (username.equals(name)){
             filterChain.doFilter(servletRequest, servletResponse);
         }else {
            dispatcher.forward(servletRequest, servletResponse);
         }
     }
+
     @Override
     public void destroy() {
         System.out.println("Destroy filter!");
